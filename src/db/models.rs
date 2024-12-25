7dfd9e654360499cc7e71883ab34a8cc17fb5541
Bhavishya <bhavishya1102@gmail.com>
2024-12-25 21:51:59 +0530

    add video with meta model

--- src/db/models.rs
+++ src/db/models.rs
@@ -3,7 +3,7 @@ use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable, Clone)]
 #[diesel(table_name = crate::db::schema::videos)]
 pub struct Video {
     pub id: Uuid,
@@ -15,7 +15,7 @@ pub struct Video {
     pub updated_at: NaiveDateTime,
 }
 
-#[derive(Debug, Serialize, Deserialize, Queryable, Insertable)]
+#[derive(Debug, Serialize, Deserialize, Queryable, Insertable, Clone)]
 #[diesel(table_name = crate::db::schema::video_qualities)]
 pub struct VideoQuality {
     pub id: Uuid,
@@ -24,4 +24,13 @@ pub struct VideoQuality {
     pub bitrate: String,
     pub file_path: String,
     pub created_at: NaiveDateTime,
+}
+
+#[derive(Debug, Serialize)]
+pub struct VideoWithMeta {
+    #[serde(flatten)]
+    pub video: Video,
+    pub qualities: Vec<VideoQuality>,
+    pub thumbnail_url: String,
+    pub stream_url: String,
 }
